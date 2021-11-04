f9e554d8ad29cd855a763023737a73af6e1f88f9
Ana Ventura <anaventura1811@gmail.com>
2021-11-03 22:10:47 -0300

    style: configura variáveis de estilo na pasta themes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Home } from './src/screens/Home';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121214',
     alignItems: 'center',
     justifyContent: 'center',
   },
